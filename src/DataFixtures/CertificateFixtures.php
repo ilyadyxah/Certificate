69c9c1dd5d9f641f3d48dbff4c918902a8ddc7ff
илья черницын <ilyadyxah@gmail.com>
2022-04-09 15:20:34 +0300

    В фикстуры добавлена генерация файла

--- src/DataFixtures/CertificateFixtures.php
+++ src/DataFixtures/CertificateFixtures.php
@@ -3,23 +3,43 @@
 namespace App\DataFixtures;
 
 use App\Entity\Certificate;
+use App\Service\FileUploader;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use Symfony\Component\HttpFoundation\File\File;
 
 class CertificateFixtures extends Fixture
 {
     protected Generator $faker;
     protected ObjectManager $manager;
+    private FileUploader $fileUploader;
+
+    private static array $files = [ // Имитация
+        'file-1.pdf',
+        'file-2.pdf',
+        'file-3.pdf',
+        'file-4.pdf',
+        'file-5.pdf',
+    ];
+
+    public function __construct(FileUploader $fileUploader)
+    {
+        $this->fileUploader = $fileUploader;
+    }
 
     public function load(ObjectManager $manager): void
     {
         $this->faker = Factory::create();
         $this->manager = $manager;
+
         $this->createMany(Certificate::class, 25, function (Certificate $entity) use ($manager) {
+
+            $fileName = $this->faker->randomElement(self::$files);
             $entity
-                ->setTitle($this->faker->text(30));
+                ->setTitle($this->faker->text(30))
+                ->setFilename($this->fileUploader->uploadFile(new File(dirname(dirname(__DIR__)) . '/public/files/' . $fileName)));
         });
 
         $manager->flush();
